b045995ff460fd0ab7f058e1eda0bccb57cc169f
grupiweb <grupiweb@gmail.com>
2024-12-27 12:30:06 +0100

    Header

--- index.php
+++ index.php
@@ -32,62 +32,9 @@ if (isset($_SESSION['id']) && $_SESSION['verified'] != '1') {
 <body>
   <!-- navbar -->
   <div class="container-fluid p-0">
-    <nav class="navbar navbar-expand-lg bg-info">
-      <div class="container-fluid">
-        <img src="./images/logo.png" alt="" class="logo">
-        <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent"
-          aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-          <span class="navbar-toggler-icon"></span>
-        </button>
-        <div class="collapse navbar-collapse" id="navbarSupportedContent">
-          <ul class="navbar-nav me-auto mb-2 mb-lg-0">
-            <li class="nav-item">
-              <a class="nav-link active" aria-current="page" href="index.php">Home</a>
-            </li>
-            <li class="nav-item">
-              <a class="nav-link" href="display_all.php">Products</a>
-            </li>
-              <?php
-              // Check if the user is logged in before displaying the cart link
-                if (!isset($_SESSION['id'])) {
-                  echo '<li class="nav-item"><a class="nav-link" href="register.php">Register</a></li>';
-                }
-              ?>
-            <li class="nav-item">
-              <a class="nav-link" href="#">Contact</a>
-            </li>
-            <li class="nav-item">
-            <?php
-              // Check if the user is logged in before displaying the cart link
-              if (isset($_SESSION['id'])) {
-                  // User is logged in, allow access to cart.php
-                  echo '<a class="nav-link" href="cart.php"><i class="fa-solid fa-cart-shopping"><sup>' . getCartProductNumber() . '</sup></i></a>';
-              } else {
-                  // User is not logged in, redirect to login.php
-                  echo '<a class="nav-link" href="cart.php"><i class="fa-solid fa-cart-shopping"><sup>' . getCartProductNumber() . '</sup></i></a>';
-              }
-              ?>
-            </li>
-          <li class="nav-item">
-              <a class="nav-link" href="#">
-                  Total Price: <?php totalPrice(); ?>
-              </a>
-          </li>
-          <?php
-            if(isset($_SESSION['id'])){
-              echo '<li class="nav-item"><a class="nav-link" href="logout.php">Logout</a></li>';
-            }
-            ?>
-
-          </ul>
-          <form class="d-flex" role="search" action="search_produkt.php" method="get">
-            <input class="form-control me-2" type="search" name="search_produkt" placeholder="Search" aria-label="Search">
-           <!-- <button class="btn btn-outline-light" type="submit">Search</button> -->
-            <input type="submit" value="search" name="search_produkt_data" class="btn btn-outline-light">
-          </form>
-        </div>
-      </div>
-    </nav>
+  <?php
+      include("./includes/header.php")
+    ?>
       <!-- thirrja e cart() -->
        <?php
        cart();
